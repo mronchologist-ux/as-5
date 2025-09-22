170970f79aafbae2b99cbaafb1e413f596a1a251
HARDIKBHAI <mr.onchologist@gmail.com>
2025-09-22 10:15:05 +0530

    Update assignment_5_task_2.py

--- assignment_5_task_2.py
+++ assignment_5_task_2.py
@@ -9,7 +9,8 @@ numbers = list(range(1, 11))
 
 first_five = numbers[0:5]
 reversed_first_five =first_five [::-1]
-
+print("original list:", numbers)
 print("extrected first five element:", first_five)
 print("Reversed exrected elements:", reversed_first_five)
+
 
